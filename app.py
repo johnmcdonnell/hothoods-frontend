fd6c09fa12326382fdbdbaef007016553059767d
John McDonnell <john.v.mcdonnell@gmail.com>
2013-09-22 23:23:51 -0700

    Templatized and updated neighborhood listings

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# vim: set fileencoding=utf-8
+
 import os
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
@@ -180,7 +182,45 @@ def home():
     """
     Route not found by the other routes above. May point to a static template.
     """
-    return render_template("index.html")
+    hoodprofiles = [{
+            "hoodname": "Flatiron",
+            "zipcode": "10010",
+            "mapurl": "static/images/flatiron.png",
+            "borough": "MANHATTAN",
+            "forecast": "+18%",
+            "forecast_icon": u"",
+            "median_price": "$790k",
+            "listing_photo_url": "http://thumbs.trulia-cdn.com/pictures/thumbs_4/ps.56/3/4/9/c/picture-uh=41f4cfb5375514124761694923e9df2-ps=349cf1e32589ec66df2915fa407d3a.jpg",
+            "listing_address": "33 E 22nd St #3D",
+            "listing_price": "$715,000",
+            "listing_url": "http://www.trulia.com/property/3131508837-33-E-22nd-St-3D-New-York-NY-10010#photo-2"
+        }, {
+            "hoodname": "Central harlem",
+            "zipcode": "10037",
+            "mapurl": "static/images/centralharlem.png",
+            "borough": "MANHATTAN",
+            "forecast": "+16%",
+            "forecast_icon": u"",
+            "median_price": "$185k",
+            "listing_photo_url": "http://thumbs.trulia-cdn.com/pictures/thumbs_4/ps.50/f/3/f/8/picture-uh=3c39eb60f81e57a9746535b9b84fd60-ps=f3f8db38d4c436b90d026b68abff647.jpg",
+            "listing_address": "2090 Madison Ave #4D",
+            "listing_price": "$155,000",
+            "listing_url": "http://www.trulia.com/property/3030874641-2090-Madison-Ave-4D-New-York-NY-10037#photo-2"
+        }, {
+            "hoodname": "East Midtown",
+            "zipcode": "10017",
+            "mapurl": "static/images/midtowneast.png",
+            "borough": "MANHATTAN",
+            "forecast_icon": u"",
+            "forecast": "+16%",
+            "median_price": "$1,270k",
+            "listing_photo_url": "http://thumbs.trulia-cdn.com/pictures/thumbs_4/ps.55/8/6/6/c/picture-uh=3475c567d5e3543b564b175b7b3a7f-ps=866cda7b2140d67f38882146d39a677.jpg",
+            "listing_address": "333 E 43rd St #PH4",
+            "listing_price": "$980,000",
+            "listing_url": "http://www.trulia.com/property/3028155277-333-E-43rd-St-PH4-New-York-NY-10017#photo-2"
+        }
+    ]
+    return render_template("index.html", listings=hoodprofiles)
 
 @app.route('/robots.txt')
 def robotstxt():
